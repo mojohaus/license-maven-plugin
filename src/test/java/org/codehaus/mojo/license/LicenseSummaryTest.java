807fa13d7b7809c0d7889c79d9fa519cdfc2881d
Paul Gier <pgier@redhat.com>
2011-02-03 17:40:08 +0000

    o Add some comments and fix formatting of LicenseSummaryTest
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/license-maven-plugin@13512 52ab4f32-60fc-0310-b215-8acea882cd1b

--- src/test/java/org/codehaus/mojo/license/LicenseSummaryTest.java
+++ src/test/java/org/codehaus/mojo/license/LicenseSummaryTest.java
@@ -32,50 +32,63 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class LicenseSummaryTest {
+public class LicenseSummaryTest
+{
 
+    /**
+     * Test reading the license summary xml file into ProjectLicenseInfo objects
+     */
     @Test
-    public void testReadLicenseSummary() throws IOException, SAXException, ParserConfigurationException {
-        File licenseSummaryFile = new File("src/test/resources/license-summary-test.xml");
-        Assert.assertTrue(licenseSummaryFile.exists());
-        FileInputStream fis = new FileInputStream(licenseSummaryFile);
-        List<ProjectLicenseInfo> list = LicenseSummaryReader.parseLicenseSummary(fis);
+    public void testReadLicenseSummary()
+        throws IOException, SAXException, ParserConfigurationException
+    {
+        File licenseSummaryFile = new File( "src/test/resources/license-summary-test.xml" );
+        Assert.assertTrue( licenseSummaryFile.exists() );
+        FileInputStream fis = new FileInputStream( licenseSummaryFile );
+        List<ProjectLicenseInfo> list = LicenseSummaryReader.parseLicenseSummary( fis );
         fis.close();
-        ProjectLicenseInfo dep = list.get(0);
-        Assert.assertEquals("org.codehaus.mojo", dep.getGroupId());
-        Assert.assertEquals("junk", dep.getArtifactId());
-        Assert.assertEquals("1.1", dep.getVersion());
+        ProjectLicenseInfo dep = list.get( 0 );
+        Assert.assertEquals( "org.codehaus.mojo", dep.getGroupId() );
+        Assert.assertEquals( "junk", dep.getArtifactId() );
+        Assert.assertEquals( "1.1", dep.getVersion() );
 
     }
 
+    /**
+     * Test writing license information to a license.xml file and then read this file
+     * back in to make sure it's ok.
+     */
     @Test
-    public void testWriteReadLicenseSummary() throws IOException, SAXException, ParserConfigurationException, TransformerFactoryConfigurationError, TransformerException {
+    public void testWriteReadLicenseSummary()
+        throws IOException, SAXException, ParserConfigurationException, TransformerFactoryConfigurationError,
+        TransformerException
+    {
         List<ProjectLicenseInfo> licSummary = new ArrayList<ProjectLicenseInfo>();
-        ProjectLicenseInfo dep1 = new ProjectLicenseInfo("org.test", "test1", "1.0");
-        ProjectLicenseInfo dep2 = new ProjectLicenseInfo("org.test", "test2", "2.0");
+        ProjectLicenseInfo dep1 = new ProjectLicenseInfo( "org.test", "test1", "1.0" );
+        ProjectLicenseInfo dep2 = new ProjectLicenseInfo( "org.test", "test2", "2.0" );
 
         License lic = new License();
-        lic.setName("lgpl");
-        lic.setUrl("http://www.gnu.org/licenses/lgpl-3.0.txt");
-        lic.setComments("lgpl version 3.0");
-        dep1.addLicense(lic);
-        dep2.addLicense(lic);
+        lic.setName( "lgpl" );
+        lic.setUrl( "http://www.gnu.org/licenses/lgpl-3.0.txt" );
+        lic.setComments( "lgpl version 3.0" );
+        dep1.addLicense( lic );
+        dep2.addLicense( lic );
 
-        licSummary.add(dep1);
-        licSummary.add(dep2);
+        licSummary.add( dep1 );
+        licSummary.add( dep2 );
 
-        File licenseSummaryFile = File.createTempFile("licSummary", "tmp");
-        //File licenseSummaryFile = new File( "src/test/resources/license-summary-test-2.xml" );
-        LicenseSummaryWriter.writeLicenseSummary(licSummary, licenseSummaryFile);
+        File licenseSummaryFile = File.createTempFile( "licSummary", "tmp" );
+        // File licenseSummaryFile = new File( "src/test/resources/license-summary-test-2.xml" );
+        LicenseSummaryWriter.writeLicenseSummary( licSummary, licenseSummaryFile );
 
-        Assert.assertTrue(licenseSummaryFile.exists());
-        FileInputStream fis = new FileInputStream(licenseSummaryFile);
-        List<ProjectLicenseInfo> list = LicenseSummaryReader.parseLicenseSummary(fis);
+        Assert.assertTrue( licenseSummaryFile.exists() );
+        FileInputStream fis = new FileInputStream( licenseSummaryFile );
+        List<ProjectLicenseInfo> list = LicenseSummaryReader.parseLicenseSummary( fis );
         fis.close();
-        ProjectLicenseInfo dep = list.get(0);
-        Assert.assertEquals("org.test", dep.getGroupId());
-        Assert.assertEquals("test1", dep.getArtifactId());
-        Assert.assertEquals("1.0", dep.getVersion());
+        ProjectLicenseInfo dep = list.get( 0 );
+        Assert.assertEquals( "org.test", dep.getGroupId() );
+        Assert.assertEquals( "test1", dep.getArtifactId() );
+        Assert.assertEquals( "1.0", dep.getVersion() );
 
     }
 }
