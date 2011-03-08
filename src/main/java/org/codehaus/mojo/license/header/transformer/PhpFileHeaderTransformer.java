9e81986ec94c6bff370613cccb9b73299d7d0545
Tony Chemit <tchemit@apache.org>
2011-03-08 09:46:18 +0000

    add a new file header transformer (for php)
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/license-maven-plugin@13725 52ab4f32-60fc-0310-b215-8acea882cd1b

---
+++ src/main/java/org/codehaus/mojo/license/header/transformer/PhpFileHeaderTransformer.java
@@ -0,0 +1,59 @@
+/*
+ * #%L
+ * License Maven Plugin
+ *
+ * $Id$
+ * $HeadURL$
+ * %%
+ * Copyright (C) 2008 - 2011 CodeLutin, Codehaus, Tony Chemit
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+package org.codehaus.mojo.license.header.transformer;
+
+/**
+ * Implementation of {@link FileHeaderTransformer} for PHP format.
+ *
+ * @author tchemit <chemit@codelutin.com>
+ * @author kmorin <kmorin@codelutin.com>
+ * @plexus.component role-hint="php"
+ * @since 1.0
+ */
+public class PhpFileHeaderTransformer
+    extends AbstractFileHeaderTransformer
+{
+
+    public static final String NAME = "php";
+
+    public static final String DESCRIPTION = "header transformer with php comment style";
+
+    public static final String COMMENT_LINE_PREFIX = " * ";
+
+    public static final String COMMENT_START_TAG = "<?php /*";
+
+    public static final String COMMENT_END_TAG = " */ ?>";
+
+    public PhpFileHeaderTransformer()
+    {
+        super( NAME, DESCRIPTION, COMMENT_START_TAG, COMMENT_END_TAG, COMMENT_LINE_PREFIX );
+    }
+
+    public String[] getDefaultAcceptedExtensions()
+    {
+        return new String[]{ "php" };
+    }
+
+}
