3890df07798b9f3c8605990a563f168cfedddf4e
Paul Gier <pgier@redhat.com>
2011-04-08 15:31:17 +0000

    o Turn on verbose logging automatically when debug is enabled
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/license-maven-plugin@13880 52ab4f32-60fc-0310-b215-8acea882cd1b

--- src/main/java/org/codehaus/mojo/license/DownloadLicensesMojo.java
+++ src/main/java/org/codehaus/mojo/license/DownloadLicensesMojo.java
@@ -127,14 +127,6 @@ public class DownloadLicensesMojo
     private boolean quiet;
 
     /**
-     * Show extra logging information.
-     * 
-     * @parameter default-value="false"
-     * @since 1.0
-     */
-    private boolean verbose;
-
-    /**
      * Include transitive dependencies when downloading license files.
      * 
      * @parameter default-value="true"
@@ -459,6 +451,6 @@ public class DownloadLicensesMojo
 
     public boolean isVerbose()
     {
-        return verbose;
+        return getLog().isDebugEnabled();
     }
 }
