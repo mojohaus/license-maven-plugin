3218fceead97c5b39a47430e5a22f8e26ca0290b
Dennis Lundberg <dennisl@apache.org>
2013-09-15 10:33:08 +0000

    [MLICENSE-55] Remove a part of the license header, to trigger an update of the license header. Otherwise the reported warning won't show up in the build log.
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/license-maven-plugin@18718 52ab4f32-60fc-0310-b215-8acea882cd1b

--- src/it/MLICENSE-55/src/main/java/info/gianlucacosta/tests/licensetest/App.java
+++ src/it/MLICENSE-55/src/main/java/info/gianlucacosta/tests/licensetest/App.java
@@ -11,10 +11,7 @@ package info.gianlucacosta.tests.licensetest;
  * published by the Free Software Foundation, either version 3 of the 
  * License, or (at your option) any later version.
  * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * [THIS PART OF THE HEADER WAS REMOVED, TO TRIGGER AN UPDATE OF THE LICENSE HEADER]
  * 
  * You should have received a copy of the GNU General Public 
  * License along with this program.  If not, see
