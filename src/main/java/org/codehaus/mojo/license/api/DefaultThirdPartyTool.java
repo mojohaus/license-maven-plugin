bddde95b0604764c934fe1315f6cd762b0e26a7a
Markus Kluckow <kluckow@cominto.de>
2016-02-02 12:32:15 +0100

    added missing import

--- src/main/java/org/codehaus/mojo/license/api/DefaultThirdPartyTool.java
+++ src/main/java/org/codehaus/mojo/license/api/DefaultThirdPartyTool.java
@@ -55,6 +55,7 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 import java.util.SortedMap;
 import java.util.SortedSet;
