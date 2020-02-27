5d542377e9fc91621c64900b4b756ed0fc24cc73
Wolfgang Wachsmuth <wachsmuth@cominto.de>
2020-02-27 14:16:20 +0100

    fix license javaDoc

--- src/main/java/org/codehaus/mojo/license/UnkownFileRemedy.java
+++ src/main/java/org/codehaus/mojo/license/UnkownFileRemedy.java
@@ -1,14 +1,26 @@
-/******************************************************************************
- * COMINTO GmbH
- * Klosterstr. 49
- * 40211 Düsseldorf
- * Germany
- *
- * (c) Copyright 2020 by COMINTO GmbH
- * ALL RIGHTS RESERVED
- *
- ******************************************************************************/
 package org.codehaus.mojo.license;
+
+/*
+ * #%L
+ * License Maven Plugin
+ * %%
+ * Copyright (C) 2008 - 2011 CodeLutin, Codehaus, Tony Chemit, Tony chemit
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
 
 import org.apache.maven.plugin.MojoExecutionException;
 
