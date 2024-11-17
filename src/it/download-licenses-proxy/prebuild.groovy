1407aaa5426119371210e6ee20fae3aca8915e4d
Slawomir Jaranowski <s.jaranowski@gmail.com>
2024-11-17 11:41:10 +0100

    Use WireMock in integration test

---
+++ src/it/download-licenses-proxy/prebuild.groovy
@@ -0,0 +1,47 @@
+import com.github.tomakehurst.wiremock.client.WireMock
+
+/*
+ * #%L
+ * License Maven Plugin
+ * %%
+ * Copyright (C) 2008 - 2011 CodeLutin, Codehaus, Tony Chemit, Tony chemit
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration
+import com.github.tomakehurst.wiremock.common.ConsoleNotifier
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo
+import static com.github.tomakehurst.wiremock.client.WireMock.get
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor
+import static com.github.tomakehurst.wiremock.client.WireMock.configureFor
+
+WireMockServer wireMockServer = new WireMockServer(WireMockConfiguration.options()
+        .port(18080)
+        .notifier(new ConsoleNotifier(false)))
+wireMockServer.start()
+
+configureFor(wireMockServer.port())
+
+stubFor(get('/licenses/LICENSE-2.0.txt')
+        .withHeader('Host', equalTo('www.apache.org'))
+        .willReturn(aResponse().withStatus(200).withBody('Proxied via WireMock')))
+
+context.put("wireMockServer", wireMockServer)
+true
